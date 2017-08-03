db1a8a763993025f0afaf06cc96c3fcc67cfeefe
Peng <605144885@qq.com>
2017-08-03 09:00:50 +0800

    去掉摘要
    丟掉摘要列

--- js.js
+++ js.js
@@ -59,13 +59,14 @@ addS();
 
 function addS() {
 	var url = path;
-	if(!hisFlag) {
+	if (!hisFlag) {
 		url += "/disclosure/" + column + "_" + tabName;
 	} else {
 		url += "/announcement/query";
 	}
 	var total = Math.ceil(66926 / 30);
-	for(let pageNum = 1; pageNum <= total; pageNum++) {
+	var throwAmount = 0;
+	for (let pageNum = 1; pageNum <= total; pageNum++) {
 		//pageNum += 1;
 		$("#pageNum_hidden_input").val(pageNum);
 		$.ajax({
@@ -73,29 +74,33 @@ function addS() {
 			type: 'POST',
 			data: $('#AnnoucementsQueryForm').serialize(),
 			dataType: 'json',
-			error: function() {
+			error: function () {
 				dataLoadingProgress(tabName, 1);
 			},
-			success: function(result) {
-				if(result != null) {
-					for(var i of result.announcements) {
+			success: function (result) {
+				if (result != null) {
+					for (var i of result.announcements) {
+						if (i.announcementTitle.indexOf('摘要') != -1) {
+							throwAmount++;
+							continue;
+						}
 						var date = fomatDate(i.announcementTime);
-						for(var l = 0; l < 6 - i.secCode.length; l++)
+						for (var l = 0; l < 6 - i.secCode.length; l++)
 							i.secCode = '0' + i.secCode;
 						var one = {
 							代码: i.secCode,
 							简称: i.secName,
 							公告标题: i.announcementTitle,
 							公告时间: date,
-							年度报告网址: 'http://www.cninfo.com.cn/cninfo-new/disclosure/szse/bulletin_detail/true/' + i.announcementId + '?announceTime=' + date
+							年度报告网址: 'http://www.cninfo.com.cn/cninfo-new/disclosure/szse/download/' + i.announcementId + '?announceTime=' + date
 						};
 						mfuck.push(one);
 					}
 
-					if(mfuck.length == total * 30)
+					if ((mfuck.length+throwAmount) == total * 30)
 						downloadFile('ok.xls', head + '<body><table>' + createTable(mfuck).innerHTML + '</table></body></html>')
 					else
-						console.log('' + ((mfuck.length / (total * 30) * 100).toFixed(2) + '%'));
+						console.log('' + (((mfuck.length+throwAmount) / (total * 30) * 100).toFixed(2) + '%'));
 				}
 			}
 		});
@@ -106,9 +111,9 @@ function addS() {
 		ctable.innerHTML = '';
 		init(json, ctable);
 
-		for(var i in json) {
+		for (var i in json) {
 			var tr = document.createElement('tr');
-			for(var j in json[i]) {
+			for (var j in json[i]) {
 				var td = document.createElement('td');
 				td.textContent = json[i][j];
 				//			td.style.width = '100px';
@@ -121,7 +126,7 @@ function addS() {
 
 	function init(json, ctable) {
 		var thr = document.createElement('tr');
-		for(var i in json[0]) {
+		for (var i in json[0]) {
 			var th = document.createElement('th');
 			th.textContent = i;
 			thr.appendChild(th);
@@ -141,14 +146,14 @@ function addS() {
 var url = 'http://www.cninfo.com.cn/cninfo-new/disclosure/szse/download/1203746968?announceTime=2017-08-01';
 var urlRange = [1, 1];
 var hack = {};
-hack.get = function(urlRange) {
+hack.get = function (urlRange) {
 
 }
-hack.getUrl = function(urlRange) {
+hack.getUrl = function (urlRange) {
 
-	return new Promise(function(resolve, reject) {
+	return new Promise(function (resolve, reject) {
 		var url = path;
-		if(!hisFlag) {
+		if (!hisFlag) {
 			url += "/disclosure/" + column + "_" + tabName;
 		} else {
 			url += "/announcement/query";
@@ -156,7 +161,7 @@ hack.getUrl = function(urlRange) {
 		var total = (urlRange[1] - urlRange[0] + 1) * 30;
 		var finishAmount = 0;
 		var failURL = [];
-		for(let pageNum = urlRange[0]; pageNum <= urlRange[1]; pageNum++) {
+		for (let pageNum = urlRange[0]; pageNum <= urlRange[1]; pageNum++) {
 			//pageNum += 1;
 			$("#pageNum_hidden_input").val(pageNum);
 			$.ajax({
@@ -164,14 +169,14 @@ hack.getUrl = function(urlRange) {
 				type: 'POST',
 				data: $('#AnnoucementsQueryForm').serialize(),
 				dataType: 'json',
-				error: function() {
+				error: function () {
 					dataLoadingProgress(tabName, 1);
 				},
-				success: function(result) {
-					if(result != null) {
-						for(var i of result.announcements) {
+				success: function (result) {
+					if (result != null) {
+						for (var i of result.announcements) {
 							var date = fomatDate(i.announcementTime);
-							for(var l = 0; l < 6 - i.secCode.length; l++)
+							for (var l = 0; l < 6 - i.secCode.length; l++)
 								i.secCode = '0' + i.secCode;
 							var one = {
 								代码: i.secCode,
@@ -180,14 +185,19 @@ hack.getUrl = function(urlRange) {
 								公告时间: date,
 								年度报告网址:
 							};
-							var url = 'http://www.cninfo.com.cn/cninfo-new/disclosure/szse/download/' + i.announcementId + '?announceTime=' + date;
-							hack.downloadFile(url).then(function() {
+							if ()
+								var url = 'http://www.cninfo.com.cn/cninfo-new/disclosure/szse/download/' + i.announcementId + '?announceTime=' + date;
+							hack.downloadFile(url).then(function () {
 								finishAmount++;
 								console.log((finishAmount / (total) * 100).toFixed(2) + '%');
-							}, function(info) {
+								if (finishAmount == total)
+									console.log(failURL);
+							}, function (info) {
 								failURL.push(info);
 								finishAmount++;
 								console.log((finishAmount / (total) * 100).toFixed(2) + '%');
+								if (finishAmount == total)
+									console.log(failURL);
 							})
 						}
 
@@ -198,18 +208,18 @@ hack.getUrl = function(urlRange) {
 
 	});
 }
-hack.downloadFile = function(url) {
+hack.downloadFile = function (url) {
 	var xhr = new XMLHttpRequest();
 
-	if(xhr.response) {
-		return new Promise(function(resolve, reject) {
+	if (xhr.response) {
+		return new Promise(function (resolve, reject) {
 			xhr.open('GET', url, true); //get请求，请求地址，是否异步
 			xhr.responseType = "blob";
 
-			xhr.onload = function() {
-				if(xhr.status == 200) {
+			xhr.onload = function () {
+				if (xhr.status == 200) {
 					var blob = xhr.response; // 注意:不是oReq.responseText
-					if(blob) {
+					if (blob) {
 						var aLink = document.createElement('a');
 						aLink.download = 'sss.PDF';
 						aLink.href = URL.createObjectURL(blob);
@@ -222,7 +232,7 @@ hack.downloadFile = function(url) {
 					reject(url);
 				}
 			}
-			xhr.onerror = function(e) {
+			xhr.onerror = function (e) {
 				console.log(e);
 				reject(url);
 			}
@@ -230,18 +240,18 @@ hack.downloadFile = function(url) {
 		});
 
 	} else {
-		return new Promise(function(resolve, reject) {
+		return new Promise(function (resolve, reject) {
 			xhr.open('GET', url, false);
 			//XHR binary charset opt by Marcus Granado 2006 [http://mgran.blogspot.com]
 			xhr.overrideMimeType('text/plain; charset=x-user-defined');
 			xhr.send(null);
-			if(xhr.status != 200) {
+			if (xhr.status != 200) {
 				reject(url);
 				console.log('error');
 			}
 			var n = xhr.responseText.length;
 			var u8arr = new Uint8Array(n);
-			while(n--) {
+			while (n--) {
 				u8arr[n] = xhr.responseText.charCodeAt(n);
 			}
 			var blob = new Blob([u8arr]);
@@ -257,14 +267,14 @@ hack.downloadFile = function(url) {
 }
 
 function loadImageAsync(url) {
-	return new Promise(function(resolve, reject) {
+	return new Promise(function (resolve, reject) {
 		var image = new Image();
 
-		image.onload = function() {
+		image.onload = function () {
 			resolve(image);
 		};
 
-		image.onerror = function() {
+		image.onerror = function () {
 			reject(new Error('Could not load image at ' + url));
 		};
 
